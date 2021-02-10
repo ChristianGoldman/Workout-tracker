5e1ff6ed5da03d5e3eda1bbb8d7b0b3da626f8bc
Christian Goldman <christian.goldman9638@gmail.com>
2021-02-10 12:48:59 -0600

    Html routes setup, app running, working on created workout api route

---
+++ models/workout.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          // cardio, resistance...etc
+          type: String,
+          trim: true,
+          required: "Enter an excercise type"
+        },
+
+        name: {
+          // Mondays workout, Weekend bulk....etc
+          type: String,
+          trim: true,
+          required: "Enter an excercise name"
+        },
+
+        duration: {
+          type: Number,
+          required: "Enter an excercise duration"
+        },
+
+        weight: {
+          type: Number
+        },
+
+        reps: {
+          type: Number
+        },
+
+        sets: {
+          type: Number
+        },
+
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  }
+)
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/workouts", async (req, res) => {
+   const data = await db.Workout.find({})    
+    res.json(data)
+  });
+
+  app.get("/api/workouts/range", async (req, res) => {
+    const data = await db.Workout.find({}).limit(7)
+    res.json(data)
+  });
+  
+  app.post("/api/workouts", async (req, res) => {
+    const day = await db.Workout.create({})
+    res.json(day)
+  });
+
+  app.put("/api/workouts/:id", async ({body, params}, res) => {
+    console.log(body)
+    const workoutUpdated = await db.Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, upsert: true, useFindandModify: false },
+    );
+      console.log(workoutUpdated);
+    res.json(workoutUpdated)
+  })};
