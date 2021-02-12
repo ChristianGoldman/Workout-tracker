3b89a9395a85e0e9c9f0d0599b1f0ac1ffaa5a8c
Christian Goldman <christian.goldman9638@gmail.com>
2021-02-11 18:20:15 -0600

    fixed env file

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+require("dotenv").config();
+
 app.use(morgan("dev"));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify:false
 })
 
 require("./routes/apiRoutes")(app);
